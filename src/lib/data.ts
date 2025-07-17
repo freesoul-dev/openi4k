d2cf08963aea91938c1507735a9dff7df8085a55
freesoul.dev <freesoul.dev@gmail.com>
2025-07-17 15:30:07 -0400

    cdn change

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -12,13 +12,13 @@ export interface Video {
 export const videos: Video[] = [
     {
         id: '1',
-        src: 'https://freesoul-cam.b-cdn.net/snb_20250615.mp4',
+        src: 'https://bzysov8ydzptmtf9.public.blob.vercel-storage.com/snb_20250615.mp4',
         description: 'Slappers and Bangers, June 15th 2025, Pittsburgh, PA',
         category: 'Events',
     },
     {
         id: '2',
-        src: 'https://freesoul-cam.b-cdn.net/belvs_20250607.mp4',
+        src: 'https://bzysov8ydzptmtf9.public.blob.vercel-storage.com/belvs_20250607.mp4',
         description: 'Pink Pony Club, June 7th 2025, Pittsburgh, PA',
         category: 'Events'
     }
